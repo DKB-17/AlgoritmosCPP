5ffb2178d2c61f53b138d0bd9853db1d98603e46
diego <136401880+DKB-17@users.noreply.github.com>
2023-12-28 17:40:09 -0300

    Inserir na pilha - incompleto

--- Pilha/algoritmoPilha.cpp
+++ Pilha/algoritmoPilha.cpp
@@ -14,15 +14,46 @@ struct pilha{
 };
 
 void criar_pilha(pilha *pilha);
+void inserir_no_topo(pilha *pilha, int num);
 
 int main(){
 
     pilha pilha;
     criar_pilha(&pilha);
 
+    int valor, opcao;
+
+    do {
+
+        cout << "0 - Sair\n" << "1 - Inserir no Topo\n" << "2 - Remover no Topo\n" << "3 - Imprimir Pilha" << endl;
+        cin >> opcao;
+
+        switch (opcao)
+        {
+        case 1:
+            cout << "Digite o valor a ser inserido:";
+            cin >> valor;
+            inserir_no_topo(&pilha, valor);
+            break;
+        default:
+            break;
+        }
+    }
+    while (opcao != 0)
+
 }
 
 void criar_pilha(pilha *pilha){
     pilha->inicio;
     pilha->tamanho = 0;
+}
+
+void inserir_no_topo(pilha *pilha, int num) {
+    no *novo = (no*) malloc(sizeof(no));
+
+    if (pilha->inicio == NULL) {
+        novo.valor = num;
+        novo.proximo = pilha->inicio;
+        pilha->inicio = novo;
+    }
 }
