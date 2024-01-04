88994aaf2b48c3af61689648707473c2b06a356c
diego <136401880+DKB-17@users.noreply.github.com>
2024-01-03 21:41:16 -0300

    Modificacao Merge Sort

--- MergeSort/main.cpp
+++ MergeSort/main.cpp
@@ -3,27 +3,29 @@
 using namespace std;
 
 void merge_sort(int vetor[], int ini, int fim);
-void imprimir_vetor(int vetor[], int tamanho);
+void imprimir_vetor(int vetor[]);
 void merge(int vetor[], int ini, int meio, int fim);
+
+const int tamanho = 10;
 
 int main(){
 
-    int tamanho = 10;
+
     int vetorA[tamanho] = {9,1,3,7,2,6,4,8,5,0};
 
-    imprimir_vetor(vetorA, tamanho);
+    imprimir_vetor(vetorA);
 
     cout << "--Ordenando--" << endl;
     merge_sort(vetorA, 0, tamanho);
 
-    imprimir_vetor(vetorA, tamanho);
+    imprimir_vetor(vetorA);
 
 
     return 0;
 }
 
 void merge_sort(int vetor[], int ini, int fim) {
-    if(ini < (fim - 1)) {
+    if(ini < fim - 1) {
         int meio = (ini+fim) / 2;
         merge_sort(vetor, ini, meio);
         merge_sort(vetor, meio, fim);
@@ -42,24 +44,33 @@ void merge(int vetor[], int ini, int meio, int fim){
     
     while (i < meio && j < fim) {
         if (vetor[i] <= vetor[j]) {
-            w[k++] = vetor[i++];
+            w[k] = vetor[i];
+            i++;
         }else{
-            w[k++] = vetor[j++];
+            w[k] = vetor[j];
+            j++;
         }
+        k++;
     }
     while (i < meio) {
-        w[k++] = vetor[i++];
+        w[k] = vetor[i];
+        k++;
+        i++;
     }
     while (j < fim) {
-        w[k++] = vetor[j++];
+        w[k] = vetor[j];
+        k++;
+        j++;
     }
     for (i = ini; i < fim; i++) {
         vetor[i] = w[i - ini];
     }
+    cout << "Vetor: " << ini << " ate " << fim << ": ";
+    imprimir_vetor(w);
     free(w);
 }
 
-void imprimir_vetor(int vetor[], int tamanho) {
+void imprimir_vetor(int vetor[]) {
 
     cout << "Vetor : [ " ;
     for(int i = 0; i < tamanho; i++) {
